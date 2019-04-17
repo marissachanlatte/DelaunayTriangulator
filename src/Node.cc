962cf887f352ce761ef2841313b71d6fff454635
Marissa Ramirez <mzweig@berkeley.edu>
2019-04-16 21:02:50 -0700

    Made Node class.

---
+++ src/Node.cc
@@ -0,0 +1,27 @@
+#include <utility>
+#include <math.h>
+#include <float.h>
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+Node::Node(){
+  m_position.first = DBL_MAX;
+  m_position.second = DBL_MAX;
+};
+
+
+Node::Node(pair<double, double> position, const int& nodeID){
+  m_position = position;
+  m_id = nodeID;
+};
+
+
+const int& Node::getID() const{
+  return m_id;
+};
+
+const pair<double, double> Node::getPosition(){
+  return m_position;
+};
---
+++ src/Node.h
@@ -0,0 +1,16 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+#include <utility> 
+using namespace std;
+
+class Node{
+  public:
+    Node();
+    Node(pair<double, double> position, const int& nodeID);
+    const int& getID() const;
+    const pair<double, double> getPosition();
+  private:
+    int m_id;
+    pair<double, double> m_position;
+};
+#endif
